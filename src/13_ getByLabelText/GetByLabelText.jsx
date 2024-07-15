15c4176464cbc35e78bdd0b0a248c328ab6ab111
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-07-15 21:11:29 +0530

    getByLabelText folder added

---
+++ src/13_ getByLabelText/GetByLabelText.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+/*
+ getByLabelText | Lec #21
+
+ ->
+
+*/
+
+const GetByLabelText = () => {
+  return (
+    <div>GetByLabelText</div>
+  )
+}
+
+export default GetByLabelText
