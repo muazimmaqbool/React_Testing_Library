2a41098401bd244765d5005a9dd4e5b02b66da4a
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-07-22 23:12:59 +0530

    TextMatch setup

---
+++ src/23_TextMatch/TextMatch.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+/*
+lec #32
+
+->Text Match with string and regex
+
+*/
+
+const TextMatch = () => {
+  return (
+    <div>TextMatch</div>
+  )
+}
+
+export default TextMatch
