84042aaa4d280d29ce279fc783fda917a9c84294
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-07-25 10:12:32 +0530

    Testing hidden elements using queryBy

--- src/24_QueryBy_QueryAllBy/QueryBy_QueryAllBy.jsx
+++ src/24_QueryBy_QueryAllBy/QueryBy_QueryAllBy.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React from "react";
 
 //lec #34
 /*
-->QueryBy and QueryAllBy:
+->queryBy and queryAllBy
 
+-> Earlier we checked the getBy Query And it was almost 10 methods that we checked: getByText,getById etc...
+-> Now we will check the queryBy query and it has almost same 10 methods like:
+        queryByText, queryById, etc...
+    Note: will will not go through all of these we see basic rest will be same
+
+->Why we use queryBy:
+    ANS: queryBy has an extra feature which getBy don't have is that if your element is hidden
+         in the ui, still you can test that element using queryBy.
+
+-> In our button example we will display Login and Logout buttons but one at a time and hide other
+  if we display login button we will hide logout button and vice versa
 */
 
+//if you want to test single element which is hidden use queryBy and if you want to test
+// multiple elements use queryAllBy
+
+//if you test the hidden element will getBy it will fail
+
 const QueryBy_QueryAllBy = () => {
+  //when login is true Logout button will be display and when false Login button will be displayed
+  let login = false;
   return (
     <div>
-        <h1>QueryBy and QueryAllBy</h1>
-    </div>
-  )
-}
+      <h1>queryBy and queryAllBy</h1>
 
-export default QueryBy_QueryAllBy
+      {login ? <button>Logout</button> : <button>Login</button>}
+    </div>
+  );
+};
+
+export default QueryBy_QueryAllBy;
--- src/24_QueryBy_QueryAllBy/QueryBy_QueryAllBy.test.js
+++ src/24_QueryBy_QueryAllBy/QueryBy_QueryAllBy.test.js
@@ -1,6 +1,14 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import QueryBy_QueryAllBy from "./QueryBy_QueryAllBy"
 
 test("Test Cases for QueryBy",()=>{
     render(<QueryBy_QueryAllBy/>)
+
+   // const btn=screen.getByText("Logout") // will will not find the button in code
+   //fails as logout is hidden now lets try with queryBy
+
+    const btn=screen.queryByText("Logout") //by this line we will check whether the button is present in the code or not
+    //expect(btn).toBeInTheDocument() ,//checks for ui
+    //it will fail if you use toBeInTheDocument() because button is present in the code
+    expect(btn).not.toBeInTheDocument() //runs, as the button is not present in the ui but in the code
 })
