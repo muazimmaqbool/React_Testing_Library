c25bbd9da60b1899172e9e44bbc4eff649c1b3e4
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-07-25 09:50:57 +0530

    TextMatch with functions

--- src/23_TextMatch/TextMatch_With_Function.jsx
+++ src/23_TextMatch/TextMatch_With_Function.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 //first check TextMatch.jsx file
 
 //lec #33
+//Text Match With Functions
 
 const TextMatch_With_Function = () => {
   return (
     <div>
         <h1>TextMatch With Function</h1>
+        <div>Hello World</div>
     </div>
   )
 }
--- src/23_TextMatch/TextMatch_With_Function.test.js
+++ src/23_TextMatch/TextMatch_With_Function.test.js
@@ -1,6 +1,22 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import TextMatch_With_Function from "./TextMatch_With_Function"
 
-Text("TextMatch With Function",()=>{
+test("TextMatch With Function",()=>{
     render(<TextMatch_With_Function/>)
+
+    //element is second argument an its optional
+    //here this callback function will check whether the text in ui/element starts with "Hello"
+   // const divEl=screen.getByText((text,element)=>text.startsWith("Hello"))
+
+   //now it will check whether the text ends with Worlds or not
+   //const divEl=screen.getByText((text,element)=>text.endsWith("World")) // ld , d : also workds
+   //const divEl=screen.getByText((text,element)=>text.endsWith("ld")) //works
+
+
+   //it will check whether the text in ui contains this text/string
+    const divEl=screen.getByText((text,element)=>{
+        return text.includes("rl")
+    })
+
+    expect(divEl).toBeInTheDocument()
 })
