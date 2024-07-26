774400f787403113e3ccb5b1e1f6e8b3f4495c01
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-07-26 21:15:27 +0530

    custom query

--- src/26_TestElements_JSQuery/TestElementsJavascriptQuery.jsx
+++ src/26_TestElements_JSQuery/TestElementsJavascriptQuery.jsx
@@ -3,13 +3,16 @@ import React from 'react'
 //lec #36
 /*
 ->Test Elements With JavaScript Query / custom query:
-    
+    we can test elements with various methods and recommended methods are:
+    getBy, queryBy, findBy and if somehow they didn't work then go for custom query or js query
+
 */
 
 const TestElementsJavascriptQuery = () => {
   return (
     <div>
         <h1>Test Elements Javascript Query</h1>
+        <div id='testId'>Hello World</div>
     </div>
   )
 }
--- src/26_TestElements_JSQuery/TestElementsJavascriptQuery.test.js
+++ src/26_TestElements_JSQuery/TestElementsJavascriptQuery.test.js
@@ -1,7 +1,12 @@
 import { render } from "@testing-library/react"
 import TestElementsJavascriptQuery from "./TestElementsJavascriptQuery"
 
-test("Testing elements with javascript query",()=>{
+test("Testing elements with javascript query|Custom Query",()=>{
     render(<TestElementsJavascriptQuery/>)
-    
+
+    //here we can give classname ".testName", id's etc
+    const divEl=document.querySelector("#testId")
+    expect(divEl).toBeInTheDocument()
+    //try for text
+    expect(divEl).toHaveTextContent("Hello World")
 })
