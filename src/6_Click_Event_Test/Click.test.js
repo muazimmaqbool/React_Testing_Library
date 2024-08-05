2d400b03f7fdadec852af95f4bad8edb1ea4a66e
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-08-05 23:11:30 +0530

    btn click testing

--- src/6_Click_Event_Test/Click.test.js
+++ src/6_Click_Event_Test/Click.test.js
@@ -3,7 +3,12 @@ import ClickTest from "./ClickTest";
 
 test("Click Event Test Case",()=>{
     render(<ClickTest/>);
-    const btn=screen.getByRole("button");//checking is button element present on the screen/getting button element
-    fireEvent.click(btn); //firing click event on button
-    expect(screen.getByText("New Data")).toBeInTheDocument(); //checking is NewData on screen after/when button is clicked
+    //checking is button element present on the screen/getting button element
+    const btn=screen.getByRole("button");
+
+    //firing click event on button
+    fireEvent.click(btn); 
+
+     //checking is NewData on screen after/when button is clicked
+    expect(screen.getByText("New Data")).toBeInTheDocument();
 })
