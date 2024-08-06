7848281729c5dcd4028aa38c3bb2e07b81f1eb79
Muazim Maqbool <muazimmaqbool4@gmail.com>
2024-08-06 22:54:15 +0530

    test case with ext specjs and specjsx

--- src/7 Different File Extensions/testCase3.spec.js
+++ src/7 Different File Extensions/testCase3.spec.js
@@ -1,6 +1,8 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import Diff_File_Ext from "./Diff_File_Ext";
 
+//test case inside file with extension .spec.js
+
 test("Click Event using .spec.js",()=>{
     render(<Diff_File_Ext/>);
     const btn=screen.getByRole("button");
--- src/7 Different File Extensions/testCase4.spec.jsx
+++ src/7 Different File Extensions/testCase4.spec.jsx
@@ -1,6 +1,8 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import Diff_File_Ext from "./Diff_File_Ext";
 
+//test case inside file with extension .spec.jsx
+
 test("Click Event using .spec.jsx",()=>{
     render(<Diff_File_Ext/>);
     const btn=screen.getByRole("button");
